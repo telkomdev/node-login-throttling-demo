0521004ad7c31d2c0ebf6051371b434c7540e948
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-12-11 05:52:12 +0700

    update limiter to express options

--- middleware/rate_limit.js
+++ middleware/rate_limit.js
@@ -6,27 +6,27 @@ class RateLimit {
             redis: options.redisClient,
             keyPrefix: 'middleware',
             points: options.points, //  options.points requests
-            duration: options.duration, // per options.duration second by IP
+            duration: options.duration, // per options.duration /interval second by IP
         });
     }
 
     setRateLimiterMiddleware() {
         return (req, res, next) => {
           // restify user
-          const ip = req.connection.remoteAddress || req.headers['x-forwarded-for'];
-          this.rateLimiter.consume(ip)
+          // const ip = req.connection.remoteAddress || req.headers['x-forwarded-for'];
+          // this.rateLimiter.consume(ip)
 
           // express user
-          // const ip = req.ip || req.headers['x-forwarded-for'];
-          //this.rateLimiter.consume(req.ip)
+          const ip = req.ip || req.headers['x-forwarded-for'];
+          this.rateLimiter.consume(req.ip)
             .then(() => {
               next();
             })
             .catch(() => {
               // restify user
-              res.send(429, {'message': 'too many requests'});
+              //res.send(429, {'message': 'too many requests'});
               // express user
-              // res.status(429).json({'message': 'too many requests'});
+              res.status(429).json({'message': 'too many requests'});
             });
         };
     }
